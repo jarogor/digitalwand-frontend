6ab45e05f9bd3848e70c0d24188af28c7f33d91e
Egor Chernyshov <jarogor@gmail.com>
2018-05-19 14:28:09 +0300

    fix path images

--- dev/temp/mocksData.js
+++ dev/temp/mocksData.js
@@ -65,10 +65,10 @@
 
 'slider': {
         items: [
-            { url: '/static/img/content/slides/1.jpg' },
-            { url: '/static/img/content/slides/2.jpg' },
-            { url: '/static/img/content/slides/3.jpg' },
-            { url: '/static/img/content/slides/4.jpg' }
+            { url: 'static/img/content/slides/1.jpg' },
+            { url: 'static/img/content/slides/2.jpg' },
+            { url: 'static/img/content/slides/3.jpg' },
+            { url: 'static/img/content/slides/4.jpg' }
         ]
     }
 ,
